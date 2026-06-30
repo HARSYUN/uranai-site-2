f8dbfe78f90d821cb8fae82a93798b54e6ea89f5
HARSYUN <shunsuke.matsubara@1st-success.com>
2026-06-30 16:01:07 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -129,7 +129,7 @@ app.post('/login', (req, res) => {
 })
 
 /*    サーバー起動 */
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
